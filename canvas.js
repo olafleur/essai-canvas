183120ec045e44bdaafefaa88db3c8a16c103372
Olivier Lafleur <olivier.lafleur@gmail.com>
2013-11-29 00:10:01 -0500

    Dessiner un triangle de base

--- canvas.js
+++ canvas.js
@@ -6,9 +6,29 @@ window.onload = function () {
         return;
     }
 
-    var context = canvas.getContext("2d");
+    var contexte = canvas.getContext("2d");
 
-    if (!context) {
+    if (!contexte) {
         alert("Impossible de récupérer le contexte du canvas");
     }
+
+    afficherPersonnage(contexte, 0, 0);
 };
+
+function afficherPersonnage(contexte, x, y) {
+    var hauteur = 30;
+    var largeur = 30;
+
+    dessinerTriangle(contexte, x, y, hauteur, largeur);
+}
+
+function dessinerTriangle(contexte, x, y, hauteur, largeur) {
+    contexte.beginPath();
+    contexte.beginPath();
+    contexte.moveTo(x, y + hauteur);
+    contexte.lineTo(x + largeur/2, y);
+    contexte.lineTo(x + largeur, y + hauteur);
+    contexte.lineTo(x, y + hauteur);
+    contexte.stroke();
+    contexte.closePath();
+}
